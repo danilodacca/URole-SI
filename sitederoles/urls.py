c09f79ac950ef98d20e6ce0de89100740a48b60e
gustaribeiro8 <gustavo.ribeiro10@usp.br>
2023-12-08 00:02:57 -0300

    renomeia pasat

--- sitederoles/urls.py
+++ sitederoles/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for sitedereceitas project.
+URL configuration for sitederoles project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
